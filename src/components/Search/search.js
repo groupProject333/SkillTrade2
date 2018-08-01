4ee636c0498aed678bdedd3c41d23552cc2bee95
thorc <chelsgieck@gmail.com>
2018-08-01 19:07:03 -0400

    style

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Jumbotron, Button, Container, Row, Col, Form, FormGroup, Label, Input, FormText, Card} from 'reactstrap';
+import { Jumbotron, Button, Row, Col, Form, FormGroup, Label, Input, FormText, Card, Container, CardHeader, CardBody} from 'reactstrap';
 import "./search.css";
 
 const Search = (props) => {
     return (
-        <section id="back">
-            <Jumbotron id="jumboText">
-                <Card id="contain">
-                
+        <Container id="search">
+            <Card id="contain">
+            <CardHeader>
                     <h2 className="text-center text-primary">
                         Welcome to Skill-Trade!
                     </h2>
+                    </CardHeader>
+                    <CardBody>
                     <Form>
                         <FormGroup>
                             {/* <Label for="search" id="label">Search</Label> */}
-                            <Input type="text" name="search" id="search" placeholder="Search Listings" large className = "text-center"/>
+                            <Input type="text" name="search" id="field" placeholder="Search Listings" large className = "text-center"/>
                         </FormGroup>
                         <Button id="btn">Submit</Button>
                         </Form>
+                        </CardBody>
                 </Card>
-            </Jumbotron>
-        </section>
+        </Container>
     );
 };
 
