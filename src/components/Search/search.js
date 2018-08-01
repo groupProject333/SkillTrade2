b6c43b58aae8e77bd838a9dd9d0ba309b003431f
thorc <chelsgieck@gmail.com>
2018-08-01 15:48:49 -0400

    search

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -4,23 +4,20 @@ import "./search.css";
 
 const Search = (props) => {
     return (
-        <section id="back">
-            <Jumbotron id="jumboText">
-                <Card id="contain">
-                
-                    <h2 className="text-center text-primary">
-                        Welcome to Skill-Trade!
-                    </h2>
-                    <Form>
-                        <FormGroup>
-                            {/* <Label for="search" id="label">Search</Label> */}
-                            <Input type="text" name="search" id="search" placeholder="Search Listings" large className = "text-center"/>
-                        </FormGroup>
-                        <Button id="btn">Submit</Button>
-                        </Form>
-                </Card>
-            </Jumbotron>
-        </section>
+        <Container id="backDiv">
+            <Card id="searchTxt">    
+                <h2 className="text-center">
+                    Welcome to Skill-Trade!
+                </h2>
+                <Form>
+                    <FormGroup>
+                        {/* <Label for="search" id="label">Search</Label> */}
+                        <Input type="text" name="search" id="search" placeholder="Search Listings" large className = "text-center"/>
+                    </FormGroup>
+                    <Button id="btn">Submit</Button>
+                </Form>
+            </Card>
+        </Container>
     );
 };
 
